5c4ea5d876e9ceafb5412cc4cf38c6ce580b51af
Dan-Riz3a <dan.rizea@aggranda.com>
2023-07-18 22:54:49 +0300

    Deleted miscommited folder

--- financial-ai/financial_ai/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Initialize routes"""
-from flask import Blueprint
-from financial_ai.routes.economic_activities import economic_activities_bp
-import financial_ai.handlers 
-
-# Create a Blueprint for the routes
-routes_bp = Blueprint('routes', __name__)
-
-routes_bp.register_blueprint(economic_activities_bp)
