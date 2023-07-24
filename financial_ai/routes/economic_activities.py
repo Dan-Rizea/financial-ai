21acc922716d6b7493e98323315574127d833d17
Dan-Riz3a <dan.rizea@aggranda.com>
2023-07-24 23:05:46 +0300

    Configured ChromaDB

--- financial_ai/routes/economic_activities.py
+++ financial_ai/routes/economic_activities.py
@@ -1,6 +1,7 @@
 """economic-activities API"""
 from flask import Blueprint, request, jsonify
-from financial_ai.handlers import process_economic_activities, import_chunk_pdfs
+from financial_ai.handlers import process_economic_activities, retriever_handler
+from financial_ai.scripts import initialize_vectoral_database
 
 economic_activities_bp = Blueprint('economic_activities', __name__)
 
@@ -15,5 +16,5 @@ async def get_economic_activities():
 async def get_embeddings():    
     """Embeds and gets CAEN codes VERY INEFFICIENT"""
     json_data = request.get_json()
-    result = await import_chunk_pdfs.embed_search_pdf(json_data.get('prompt'))
+    result = await retriever_handler.retrieve_embedded_data(json_data.get('prompt'))
     return jsonify({"output": result})
