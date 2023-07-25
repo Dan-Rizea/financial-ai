d3597e2dbf690ba3409efe97120269227de2ecd9
Dan-Riz3a <dan.rizea@aggranda.com>
2023-07-25 23:59:30 +0300

    Commit for deployment

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Run server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
     
